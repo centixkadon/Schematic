1da971cdc4224549a87f2822e2f16eba246e1a0e
ZhangYan <youngzhang95@163.com>
2017-11-20 20:18:32 +0800

    Fix schematic

--- js/schematic/schematic.js
+++ js/schematic/schematic.js
@@ -4,51 +4,39 @@
 //3.drawComponent的返回值不是<g>对象，而是对应的元器件结构体。
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
+//用来存放电路图中所有元器件的数组
+let schSchematic = [];
 
-//用函数方法定义元器件结构体，包括name、x、y以及两个内部函数
-function schComponent(name, x, y) {
-  this.name = name;
-  this.x = x;
-  this.y = y;
-  this.moveTo = function (X, Y) {
-    this.x = X;
-    this.y = Y;
-    drawSchComponent(this);
-    return this;
-  };
-  this.moveBy = function (X, Y) {
-    this.x = this.x + X;
-    this.y = this.y + Y;
-    drawSchComponent(this);
-    return this;
-  };
+jQuery.prototype.moveTo = function (X, Y) {
+  this.attr('transform', 'translate(' + X + ' ' + Y + ')');
+  this.schX = X;
+  this.schY = Y;
+  return this;
 }
 
-//用来存放电路图中所有元器件的数组
-let schSchematic = new Array();
+jQuery.prototype.moveBy = function (X, Y) {
+  X = X + this.schX;
+  Y = Y + this.schY;
+  this.attr('transform', 'translate(' + X + ' ' + Y + ')');
+  this.schX = X;
+  this.schY = Y;
+  return this;
+}
+
+jQuery.prototype.removeIt = function () {
+  schSchematic[this.schIdx] = undefined;
+  this.remove();
+  return this;
+}
 
 //输入obj为schSchematic数组元素,画出对应的元器件并返回创建的<g>对象
-function drawSchComponent(obj) {
-  let group = $s('g').appendComponent(obj.name).attr('transform', 'translate(' + obj.x+' '+obj.y + ')');
-  $('#svgSch').append(group);
-  return group;
-}
-
-//保存一个元器件名称、位置，返回当前元器件数组schSchematic的元素个数
-function writeComponent(componentName,offsetX,offsetY) {
-  let component=new schComponent(componentName,offsetX,offsetY);
-  return schSchematic.push(component);
-}
-
-//画出一个元器件，返回元器件结构体
 function drawComponent(componentName, offsetX, offsetY) {
-  let idx = writeComponent(componentName, offsetX, offsetY)-1;
-  drawSchComponent(schSchematic[idx]);
-  return schSchematic[idx];
-}
-
-function drawSchematic() {
-  for (component in schSchematic) {
-    drawSchComponent(schSchematic[component]);
-  }  
+  let group = $s('g').appendComponent(componentName).attr('transform', 'translate(' + offsetX + ' ' + offsetY + ')');
+  group.schName = componentName;
+  group.schIdx = schSchematic.length;
+  group.schX = offsetX;
+  group.schY = offsetY;
+  $('#svgSch').append(group);
+  schSchematic.push(group);
+  return group;
 }

18729fda4c35d4d371d6eda17aefacd8fb881830
Seki7 <729427695@qq.com>
2017-11-22 16:46:53 +0800

    Update js/components/components.js

--- js/components/components.js
+++ js/components/components.js
@@ -15,3 +15,8 @@ let schComponents = {
 */
 
 let schComponents = {};
+
+/*使用如下方法对schComponents进行添加
+  defineComponent('vcc').has('line', [-10, -10, 10, -10]).has('line', [0, -10, 0, 0]);
+  defineComponent('gnd').has('line', [0, 0, 0, 10], [-15, 10, 15, 10], [-10, 15, 10, 15], [-5, 20, 5, 20]);
+*/

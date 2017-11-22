cc3b153a417bcab2c927bcaa3a3753154850b7b6
AndyLiming <mingli6250@163.com>
2017-11-22 15:27:02 +0800

    update events.js

--- js/events/events.js
+++ js/events/events.js
@@ -12,59 +12,60 @@ function initEvents() {
   });
 
   //init button for gnd
-  let gndButton = drawButton('gnd');
-  gndButton.mousedown(function () {
+  /*let gndButton = drawButton('gnd');
+  gndButton.mousedown(function (ev) {
     comp = 'gnd';
-    compModel = drawComponent(comp, 10, 10);
     state = 1;
-  });
-
+    compModel = drawComponent(comp, 10, 10);
+    addEvents(compModel, ev);
+  });*/
 
   //components move with mouse
   $('#svg').mousemove(function (ev) {
     if (state == 1) {
-      [x, y] = getMousePosition(ev);
-      //compModel.moveTo(x, y);
+      [x, y] = getSchPos(ev);
       compModel.moveTo(x, y);
     }
     else if (state == 2) {
-      [x, y] = getMousePosition(ev);
+      [x, y] = getSchPos(ev);
       //compModel.moveBy(x, y);
       compModel.moveTo(x, y);
     }
   });
 
-
   //components embedded
   $('#svg').mouseup(function (ev) {
-    [x, y] = getMousePosition(ev);
+    [x, y] = getSchPos(ev);
     if ((state == 1 || state == 2) && x>0) {
       state = 0;
       comp = '';
     }
   });
-  $('#svg').keydown(function(ev) {
-  if (ev.keyCode == 27) {
-    drawComponent('vcc', 50, 50);
-  }
-  });
+
+  
 }
 
 
 /*private functions for this modulw*/
-//get coordinates of mouse
-function getMousePosition(ev) {
-  let x = getSvgPos(ev.offsetX) - 100, y = getSvgPos(ev.offsetY);
-  return [x, y];
-}
 //events for compModel
 function addEvents(compModel, ev) {
   compModel.mousedown(function () {
-    if (state == 0 || moveAllow == 1) {
+    if (state == 0) {
       state = 2;
     }
   });
-  $('#svg').keydown(function () {
-    state = 0;
+  $('body').keydown(function (ev) {
+    if (state == 2) {
+      switch (ev.which){
+        case 27:
+          state = 0;
+          break;
+        case 46:
+          state = 0;
+          compModel.removeIt();
+          break;
+
+      }
+    }
   });
 }

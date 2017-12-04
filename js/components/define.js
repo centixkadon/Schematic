a7e674a2a805e714df841f111f18d62842e32c0a
朱昊川 <1525722736@qq.com>
2017-12-04 22:50:11 +0800

    update define.js

--- js/components/define.js
+++ js/components/define.js
@@ -35,41 +35,47 @@ Array.prototype.has = function (name) {
       break;
     */
     case "polyline":
-      var temp = new Object();
-      temp.mark = name;
-      temp.attr = new Object();
-      temp.attr.points = arguments[1];
-      temp.attr.style = arguments[2];
-      this.push(temp);
+      if (!((typeof(arguments[1]) === "string") && (arguments.length === 2))) {
+          throw ".has('polyline', 'x1,y1 x2,y2 ...') 使用出错，例子参照 https://github.com/centixkadon/Schematic/blob/master/js/README.md";
+      }
+      this.push({
+          mark: name,
+          attr: { points: arguments[1],},
+        });
       break;
     case "circle":
-      var temp = new Object();
-      temp.mark = name;
-      temp.attr = new Object();
-      temp.attr.cx = arguments[1];
-      temp.attr.cy = arguments[2];
-      temp.attr.r = arguments[3];
-      this.push(temp);
+      if (!(arguments.length === 4)) {
+          throw ".has('circle', cx, cy, r) 使用出错，例子参照 https://github.com/centixkadon/Schematic/blob/master/js/README.md";
+      }
+      this.push({
+          mark: name,
+          attr: { cx: arguments[1],cy:arguments[2],r:arguments[3]},
+        });
       break;
     case "polygon":
-      var temp = new Object();
-      temp.mark = name;
-      temp.attr = new Object();
-      temp.attr.points = arguments[1];
-      temp.attr.style = arguments[2];
-      this.push(temp);
+      if (!((typeof(arguments[1]) === "string") && (arguments.length === 2))) {
+          throw ".has('polygon', 'x1,y1 x2,y2 ...') 使用出错，例子参照 https://github.com/centixkadon/Schematic/blob/master/js/README.md";
+      }
+      this.push({
+          mark: name,
+          attr: { points: arguments[1],},
+        });
+
       break;
     case "rect":
-      var temp = new Object();
-      temp.mark = name;
-      temp.attr = new Object();
-      temp.attr.width = arguments[1];
-      temp.attr.height = arguments[2];
-      temp.attr.style = arguments[3];
-      this.push(temp);
+      if (!(arguments.length === 3)) {
+          throw ".has('rect', width, height) 使用出错，例子参照 https://github.com/centixkadon/Schematic/blob/master/js/README.md";
+      }
+      this.push({
+        mark: name,
+        attr: { width: arguments[1], height: arguments[2],},//点的默认半径为1
+      });
       break;
       case "points":
       for (let i = 1; i < arguments.length; ++i) {
+        if (!((arguments[i] instanceof Array) && (arguments[i].length === 2))) {
+          throw ".has('points', [x, y]) 使用出错，例子参照 https://github.com/centixkadon/Schematic/blob/master/js/README.md";
+        }
         this.push({
           mark: "circle",
           attr: { cx: arguments[i][0], cy: arguments[i][1], r: 1,},//点的默认半径为1

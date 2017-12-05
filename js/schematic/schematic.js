bca7d36cb4e5a574a420f0c6acf28f64a3b74880
ZhangYan <youngzhang95@163.com>
2017-12-05 16:33:18 +0800

    添加选择/取消（class属性）

--- js/schematic/schematic.js
+++ js/schematic/schematic.js
@@ -2,7 +2,8 @@
 //1.drawSchComponent()函数画出一个元器件、drawSchematic()画出所有已保存在schSchematic中的元器件。
 //2.因为涉及到成员函数，在这里用函数的方法定义元器件结构体。
 //3.drawComponent的返回值不是<g>对象，而是对应的元器件结构体。
-//4.this.rotateIt(deg)为旋转器件函数。
+//4.this.rotateTo(deg)/this.rotateBy(deg)为旋转器件函数。
+//5.select为对象添加一个class属性，class内容见select.css
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -38,6 +39,17 @@ $.fn.extend({
     schSchematic[this.getProps('pose').index] = undefined;
     return this.remove();
   },
+  selectIt: function () {
+    let props = this.getProps('pose');
+    props.select = true;
+    return this.addClass('selected');
+  },
+  unselectIt: function () {
+    let props = this.getProps('pose');
+    props.select = false;
+    this.removeClass('selected');
+    return this.addClass('unselect');
+  }
 });
 
 function createComponent(componentName, componentX, componentY) {

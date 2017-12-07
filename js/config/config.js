9b3532136fc6b80561315c0fb15bf6b8e395ad37
centixkadon <centixkadon@126.com>
2017-12-07 21:51:23 +0800

    Update config

--- js/config/config.js
+++ js/config/config.js
@@ -3,9 +3,9 @@ var schConfig = {
   svg: {
     button: {
       size: 50,
-      col: 2,
+      col: 3,
     },
-    innerSize: { width: 1100, height: 660 },
+    innerSize: { width: 1100, height: 550 },
   },
   componentsScale: 1,
 };
